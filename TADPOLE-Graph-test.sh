e39eb8da3d3fef3813ca813e9a0d6f5815d552b9
Taihuachen-cfair <cfair-cth@mail.sdu.edu.cn>
2025-05-07 14:25:18 +0800

    Update TADPOLE-Graph-test.sh

--- TADPOLE-Graph-test.sh
+++ TADPOLE-Graph-test.sh
@@ -1,18 +1,8 @@
-#--------------------------------------------
-# base params:
-# python main.py --dataname TADPOLE --mode local-global --marl_mode 2  --MP_mode GCN  --n_head 3 --n_hidden 21 --GC_mode weighted-cosine \
-#         --reg 0.26150702874057236 --lr 0.007325907184248944 --dropout 0.005331714042633606 \
-#         --CL_weight 0.5085609198005367 --CL_rate 0.18777487575294727  --CL_mse 1.1348030844244705\
-#         --nlayer 2 --gl_attn_layer 0 --gl_lin_layer 1 \
-#         --CL_mode random_mask_edge --seed 27750 \
-# acc:92.81
-#--------------------------------------------
-
 python main.py --dataname TADPOLE --mode local-global --marl_mode 2  --MP_mode GCN  --n_head 3 --n_hidden 21 --GC_mode weighted-cosine \
-         --reg 0.26150702874057236 --lr 0.007325907184248944 --dropout 0.005331714042633606 \
+         --reg 0.26 --lr 0.01 --dropout 0.005 \
          --nlayer 2 --gl_attn_layer 0 --gl_lin_layer 1 \
-         --CL_weight 0.5085609198005367 --CL_rate 0.18777487575294727  --CL_mse 1.1348030844244705 \
-         --CL_mode random_mask_edge --seed 27750 \
+         --CL_weight 0.5 --CL_rate 0.2  --CL_mse 0.9 \
+         --CL_mode random_mask_edge
 
 
 
